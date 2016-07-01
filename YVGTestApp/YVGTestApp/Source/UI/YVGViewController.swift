51818016e8f897bdea10212ff27aa394f08d83d7
Yevgen <yevgen.tsyganenko@idapgroup.com>
2016-07-01 16:07:23 +0300

    upd

---
+++ YVGTestApp/YVGTestApp/Source/UI/YVGViewController.swift
@@ -0,0 +1,25 @@
+//
+//  YVGViewController.swift
+//  YVGTestApp
+//
+//  Created by Yevgen on 12/30/15.
+//  Copyright © 2015 IDAP. All rights reserved.
+//
+
+import UIKit
+
+class YVGViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
